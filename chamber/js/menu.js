4f40cf50d20a06f964a266739653c70b13cd7767
LawrenceYamete <LawrenceYamete>
2022-01-30 06:12:38 +0800

    Main

---
+++ chamber/js/menu.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    document.getElementById("primarynav").classList.toggle("open"); 
+    document.getElementById("togglemenu").classList.toggle("open"); 
+}
+
+const x = document.getElementById('togglemenu');
+x.onclick = toggleMenu;
+
+
+
