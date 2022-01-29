599b709ba0faf89010c9dcc0d7ac7895ddea599d
LawrenceYamete <LawrenceYamete>
2022-01-29 12:51:12 +0800

    main

---
+++ chamber/js/dates.js
@@ -0,0 +1,16 @@
+document.querySelector('#lastmod').innerHTML = `<strong>Last Modified</strong> ${document.lastModified}`;
+
+const year = document.querySelector("#year")
+year.innerHTML = new Date().getFullYear();
+
+
+// document.querySelector('#dateaside').innerHTML = <em>${document.fulldateUK}</em>
+
+const datefieldUK = document.querySelector("date");
+
+const now = new Date();
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+
+datefieldUK.innerHTML = `<em>${fulldateUK}</em>`;
