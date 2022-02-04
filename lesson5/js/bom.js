c451313dee79f8fb2b9d8461baccd774386eb8a0
LawrenceYamete <LawrenceYamete>
2022-02-04 10:25:26 +0800

    Main

---
+++ lesson5/js/bom.js
@@ -0,0 +1,24 @@
+const list = document.querySelector('ul');
+
+const input = document.querySelector('input');
+
+const button = document.querySelector('button');
+button.addEventListener('click', function(){
+    let item = input.value;
+    input.value = '';
+
+const item_list = document.createElement('li');
+const text_list = document.createElement('span');
+const button = document.createElement('button');
+
+item_list.appendChild(text_list);
+text_list.textContent = item;
+item_list.appendChild(button);
+button.textContent = '❌';
+list.appendChild(item_list);
+
+button.addEventListener('click', function() {
+  list.removeChild(item_list);
+});
+input.focus('');
+});
