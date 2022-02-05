cd2338f6916c4da60c3588a32d2ee40ae3241f3f
LawrenceYamete <LawrenceYamete>
2022-02-06 05:14:31 +0800

    Main

--- chamber/js/menu.js
+++ chamber/js/menu.js
@@ -7,4 +7,17 @@ const x = document.getElementById('togglemenu');
 x.onclick = toggleMenu;
 
 
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://community-open-weather-map.p.rapidapi.com/weather?q=Manila%2C%20Philippines&lat=0&lon=0&callback=test&id=1217&lang=null&units=imperial&mode=xml",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+		"x-rapidapi-key": "55dd02c407msh017a9136c612aecp105a3ajsn5315ce8262f0"
+	}
+};
 
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
