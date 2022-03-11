261b59e63d5b9ebfb16a82e34473fb5136b9dc41
LawrenceYamete <LawrenceYamete>
2022-03-11 20:36:26 +0800

    main

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -19,44 +19,75 @@ function displayProphets(prophet) {
   let pic = document.createElement("picture");
 
 
-  h2.textContent = `${prophet.name} ${prophet.lastname}`;
-  p.innerHTML = `<strong>Birth Date:</strong> ${prophet.birthdate} <br/>  
+
+    h2.textContent = `${prophet.name} ${prophet.lastname}`;
+    p.innerHTML = `<strong>Birth Date:</strong> ${prophet.birthdate} <br/>  
   <strong>Birth Place:</strong> ${prophet.birthplace}`;
 
-  portrait.setAttribute("src", prophet.imageurl);
-  portrait.setAttribute(
-    "alt",
-    `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}`
-  );
-  portrait.setAttribute("loading", "lazyload");
+    portrait.setAttribute("src", "image/gray-background-7131-96d780fd18d4eaf58a7331d45573204e@1x.jpg");
+    portrait.setAttribute("data-src", prophet.imageurl);
+    portrait.setAttribute(
+      "alt",
+      `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}`
+    );
+    portrait.setAttribute("loading", "lazyload");
 
-  if (prophet.order == 1) {
-    portrait.setAttribute(
-      "alt",
-      `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}st Latter-day President`
-    );
-  } else if (prophet.order == 2) {
-    portrait.setAttribute(
-      "alt",
-      `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}nd Latter-day President`
-    );
-  } else if (prophet.order == 3) {
-    portrait.setAttribute(
-      "alt",
-      `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}rd Latter-day President`
-    );
-  } else {
-    portrait.setAttribute(
-      "alt",
-      `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`
-    );
+    
+
+    if (prophet.order == 1) {
+      portrait.setAttribute(
+        "alt",
+        `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}st Latter-day President`
+      );
+    } else if (prophet.order == 2) {
+      portrait.setAttribute(
+        "alt",
+        `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}nd Latter-day President`
+      );
+    } else if (prophet.order == 3) {
+      portrait.setAttribute(
+        "alt",
+        `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}rd Latter-day President`
+      );
+    } else {
+      portrait.setAttribute(
+        "alt",
+        `Portrait of ${prophet.name} ${prophet.lastname} - ${prophet.order}th Latter-day President`
+      );
+    }
+
+    
+
+    card.appendChild(h2);
+    card.appendChild(p);
+    card.appendChild(pic);
+    pic.appendChild(portrait);
+
+    document.querySelector("div.cards").appendChild(card);
+
+    const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+    const loadImages = (image) => {
+      image.setAttribute('src', image.getAttribute('data-src'));
+      image.onload = () => {
+        image.removeAttribute('data-src');
+      };
+    };
+    if ('IntersectionObserver' in window) {
+      const observer = new IntersectionObserver((items) => {
+        items.forEach((item) => {
+          if (item.isIntersecting) {
+            loadImages(item.target);
+            observer.unobserve(item.target);
+          }
+        });
+      });
+      imagesToLoad.forEach((img) => {
+        observer.observe(img);
+      });
+    } else {
+      imagesToLoad.forEach((img) => {
+        loadImages(img);
+      });
+    }
   }
-
-  card.appendChild(h2);
-  card.appendChild(p);
-  card.appendChild(pic);
-  pic.appendChild(portrait);
-
-  document.querySelector("div.cards").appendChild(card);
-}
-
