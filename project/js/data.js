5a5ff6a1c7b044912f434e7ab5781817bae42a61
LawrenceYamete <LawrenceYamete>
2022-04-05 08:52:06 +0800

    project

--- project/js/data.js
+++ project/js/data.js
@@ -20,16 +20,6 @@ function displayTemples(temples) {
     let p = document.createElement("p");
     let image = document.createElement("img");
 
-// ======
-    let btn = document.createElement("a");
-    let modal = document.createElement("div");
-    let i = document.createElement("i");
-    let h23 = document.createElement("h2");
-    let p1 =document.createElement("p");
-    let ac = document.createElement("a");
-    let ov = document.createElement("div");
-// ======
-
     image.setAttribute("class", "templeImgs");
     image.setAttribute("src", "images/medium/iStock-995624510-3000x1500.jpg");
     image.setAttribute("data-src", temples.imageurl);
@@ -37,60 +27,7 @@ function displayTemples(temples) {
         "alt",
         `${temples.name}`
     );
-    image.setAttribute("loading", "lazy");
-    // load
-
-// ======
-    ov.setAttribute("id", "overlay")
-    ac.setAttribute("data-close-button class", "closebtn");
-    ac.setAttribute("id", "cbtn");
-    p1.innerHTML = `lrerasdasdasdasdasdasdasdasdasdasdasd`;
-    h23.innerHTML = `pop up`;
-    i.setAttribute("class", "fas fa-times close-btn");
-    modal.setAttribute("class", "popup-box");
-    modal.setAttribute("id", "box");
-    btn.setAttribute("class", `moreInfo${temples.order}`);
-    btn.setAttribute("data-modal-target", `#modal`);
-    btn.innerHTML = (`More Info`);
-
-// ======
-
-    const openModalButtons = document.querySelectorAll('[data-modal-target]')
-    const closeModalButtons = document.querySelectorAll('[data-close-button]')
-    const overlay = document.getElementById('overlay');
-
-    openModalButtons.forEach(a => {
-        a.addEventListener('click', () => {
-          const modal = document.querySelector(a.dataset.modalTarget)
-          openModal(modal)
-        })
-      })
-      
-      overlay.addEventListener('click', () => {
-        const modals = document.querySelectorAll('.modal.active')
-        modals.forEach(modal => {
-          closeModal(modal)
-        })
-      })
-      
-      closeModalButtons.forEach(a => {
-        a.addEventListener('click', () => {
-          const modal = a.closest('.modal')
-          closeModal(modal)
-        })
-      })
-      
-      function openModal(modal) {
-        if (modal == null) return
-        modal.classList.add('active')
-        overlay.classList.add('active')
-      }
-      
-      function closeModal(modal) {
-        if (modal == null) return
-        modal.classList.remove('active')
-        overlay.classList.remove('active')
-
+    image.setAttribute("loading", "lazyload");
 
     h2.textContent = `${temples.name}`;
     p.innerHTML = `<strong>Address:</strong> ${temples.address} <br/>  
@@ -99,24 +36,27 @@ function displayTemples(temples) {
 
 
     if (temples.order == 1) {
-        btn.setAttribute("class", `moreInfo${temples.order}`);
+        image.setAttribute(
+            "alt",
+            `${temples.name}. The ${temples.order}st Temple`
+        );
     } else if (temples.order == 2) {
-        btn.setAttribute("class", `moreInfo${temples.order}`);
+        image.setAttribute(
+            "alt",
+            `${temples.name}. The ${temples.order}nd Temple`);
     } else if (temples.order == 3) {
-        btn.setAttribute("class", `moreInfo${temples.order}`);
+        image.setAttribute(
+            "alt",
+            `${temples.name}. The ${temples.order}rd Temple`);
     } else {
-        btn.setAttribute("class", `moreInfo${temples.order}`);
+        image.setAttribute(
+            "alt",
+            `${temples.name}. The ${temples.order}th Temple`);
     }
 
     card.appendChild(image);
     card.appendChild(h2);
     card.appendChild(p);
-    card.appendChild(btn);
-    card.appendChild(modal);
-    modal.appendChild(i);
-    modal.appendChild(h23);
-    modal.appendChild(p1);
-    modal.appendChild(ac);
 
     document.querySelector("div.cards").appendChild(card);
 
